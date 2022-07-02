36713bc821223a720a26ce22393555abe97c2887
Basten1209 <seungjunoh@postech.ac.kr>
2022-07-03 01:16:41 +0900

    first commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-7954824f25b08d80.d
@@ -0,0 +1,5 @@
+C:\Users\user\projects\hello_cargo\target\debug\deps\hello_cargo-7954824f25b08d80.rmeta: src\main.rs
+
+C:\Users\user\projects\hello_cargo\target\debug\deps\hello_cargo-7954824f25b08d80.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\user\projects\hello_cargo\target\debug\hello_cargo.exe: C:\Users\user\projects\hello_cargo\src\main.rs
